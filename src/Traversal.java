783111736700734401637108e850b392c46d03a4
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-05-28 11:43:54 -0700

    completed all in class livecode

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,40 @@
+import javax.swing.tree.TreeCellRenderer;
+
 public class Traversal {
   public static void main(String[] args) {
 
+    TreeNode root = new TreeNode(10);
+
+    root.left = new TreeNode(9);
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(2);
+
+    root.right = new TreeNode(15);
+    root.right.left = new TreeNode(-3);
+    root.right.right = new TreeNode(5);
+    root.right.right.right = new TreeNode(22)
+    System.out.println(preorder(10));
+  }
+
+  public static void preorder(TreeNode current) {
+    if (current == null) return;
+    System.out.println(current.data);
+
+    preorder(current.left);
+    preorder(current.right);
+  }
+
+  public static void postorder(TreeNode current) {
+    if(current==null) return;
+    postorder(current.left);
+    postorder(current.right);
+    System.out.println(current.data);
+  }
+
+  public static void inOrder(TreeNode current) {
+    if(current==null) return;
+    inOrder(current.left);
+    System.out.println(current.data);
+    inOrder(current.right);
   }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,23 @@
 public class TreeNode {
+ int data;
+ public TreeNode left;
+ public TreeNode right;
+    
 
-  
+ public TreeNode(int data) {
+    this.data = data;
+
+ }
+
+ public TreeNode (int data, TreeNode left) {
+    this.data = data;
+    this.left = left;
+ }
+
+
+ public TreeNode(int data, TreeNode left, TreeNode right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+ }
 }
